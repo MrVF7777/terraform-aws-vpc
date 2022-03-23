2447c6c5ce5858cf989f6e11c48a3fc67f8c2a9e
Vladimir Funticov <86083520+MrVF7777@users.noreply.github.com>
2022-03-23 17:57:07 -0500

    NGW remoced for now

--- route-tables.tf
+++ route-tables.tf
@@ -11,16 +11,3 @@ resource "aws_route_table" "public_table" {
   }
 }
 
-
-#Bellow is the NGW Route Table 
-
-resource "aws_route_table" "private_table" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw.id
-  }
-    tags = {
-    Name = "NGW Route"
-  }
-}
