dd5581eb1437b90b97e913619c22f04a49c965cb
Vladimir Funticov <86083520+MrVF7777@users.noreply.github.com>
2022-03-23 11:19:34 -0500

    vars edit

--- variable.tf
+++ variable.tf
@@ -3,10 +3,22 @@ variable "tags" {
     type = map
 }
 
-variable "private_subnets_cidr" {
-    type = list 
+variable "private_subnets_cidrs" {
+  description = "private subnet cidr ip's"
+  type        = list(string)
+  default     = [
+      "10.0.1.0/24",
+      "10.0.2.0/24",
+      "10.0.3.0/24",
+  ]
 }
 
-variable "public_subnets_cidr" {
-    type = list 
+variable "public_subnets_cidrs" {
+  description = "public subnet cidr ip's"
+  type        = list(string)
+  default     = [
+      "10.0.101.0/24",
+      "10.0.102.0/24",
+      "10.0.103.0/24",
+  ]
 }
